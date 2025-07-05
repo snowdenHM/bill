b143ff15af8588150f919b2db3aa57d38f59218c
Heramb Mishra <herambmishra@geniusvision.in>
2025-07-05 17:10:36 +0530

    Bill Update

--- apps/bills/tally/api/api_views.py
+++ apps/bills/tally/api/api_views.py
@@ -1,6 +1,7 @@
 import re
 from rest_framework import status
 from rest_framework import viewsets
+from collections import defaultdict
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
